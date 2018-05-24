2b3f07a9804c7b77e2042917c4602f469a267957
liroy y <liroy.yarimi@gmail.com>
2018-05-24 13:52:50 +0300

    Add images, label and complete all the design.

--- Cryptocurrency Ticker/ViewController.swift
+++ Cryptocurrency Ticker/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let baseURL = "I need to hide this line"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
